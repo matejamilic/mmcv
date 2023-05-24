709776319f795166f494fa6f32bcd8ce0cb6b036
matejamilic <134512429+matejamilic@users.noreply.github.com>
2023-05-24 14:53:36 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+    strings:["Serviser","Developer","Dizajner"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".about-content .typing", {
+    strings:["Serviser","Developer","Dizajner"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
